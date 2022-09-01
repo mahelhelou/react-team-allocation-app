a3739c362143ff856076b0e2a3545a66ff8e8cb7
mahelhelou <mahelhelou@gmail.com>
2022-09-01 16:01:38 +0300

    Completed project v01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Nav from './components/Navbar'
+import Header from './components/Header'
+import Employees from './components/Employees'
+import GroupedTeamMembers from './components/GroupedTeam'
+import NotFound from './components/NotFound'
+import Footer from './components/Footer'
+
+import { DataProvider } from './context/DataContext'
+
+const App = () => {
+	return (
+		<DataProvider>
+			<Router>
+				<Nav />
+				<Header />
+				<Routes>
+					<Route path='/' element={<Employees />}></Route>
+					<Route path='/GroupedTeamMembers' element={<GroupedTeamMembers />}></Route>
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</DataProvider>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+// Footer component
+const Footer = () => {
+	return (
+		<footer className='py-4'>
+			<div className='container'>
+				<p>Team Member Allocation App - {new Date().getFullYear()}</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
